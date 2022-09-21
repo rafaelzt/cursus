5869263df5bd438baa5a49b64762adb2b394906f
Rafael Z Tanganelli <kabelu@gmail.com>
2022-09-21 18:08:51 +0200

    part2

---
+++ libft/_ft_substr.c
@@ -0,0 +1,25 @@
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	char	*aux;
+	size_t	index;
+
+	aux = (char *)s;
+	index = 0;
+	ptr = malloc(sizeof(*s) * len);
+	if (ptr == 0)
+		return (ptr);
+	while (index != start)
+		index++;
+
+	while (len > 0)
+	{
+		ptr[]
+		len--;
+	}
+	return (0);
+
+}
