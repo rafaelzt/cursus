3bb06f33643286f6bfb1d1a6e66bb295ea27c3c1
Rafael Z Tanganelli <kabelu@gmail.com>
2022-09-30 20:38:38 +0200

    simplify makefile

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 14:13:58 by rzamolo-          #+#    #+#             */
-/*   Updated: 2022/09/30 19:10:40 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2022/09/30 20:04:16 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	else if (!(*lst))
 	{
 		*lst = new;
-		return;
+		return ;
 	}
 	last = ft_lstlast(*lst);
 	last->next = &*new;
