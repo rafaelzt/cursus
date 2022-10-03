cba3e16c9817b2fb16f58d07db25e7211f99a8b9
Rafael Z Tanganelli <kabelu@gmail.com>
2022-10-03 10:27:51 +0200

    evaluations

---
+++ libft/eval-02_/ft_putstr_fd.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 21:51:43 by rzamolo-          #+#    #+#             */
+/*   Updated: 2022/10/01 13:45:29 by rzamolo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	void ft_putstr_fd(char *s, int fd);
+
+DESCRIPTION
+    Send the string 's' to the given file descriptor 'fd'.
+*/
+void	ft_putstr_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+}
